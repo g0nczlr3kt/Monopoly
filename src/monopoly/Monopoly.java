72d022930ee3d56bee74330ef9af85f170083c4c
g0ncz <theonathan96@gmail.com>
2015-05-18 16:49:39 +0200

    Commit numéro 2

--- src/monopoly/Monopoly.java
+++ src/monopoly/Monopoly.java
@@ -16,7 +16,8 @@ public class Monopoly {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here    
+        System.out.println("Bonjour ");
+
     }
-    
 }
